c06111723035db3acf0e994ae2fa314db06fb61f
guxiang <2461479134@qq.com>
2024-02-16 20:41:47 +0800

    all

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/vlate/",
   head: [["link", { rel: "icon", href: "/logo.svg" }]],
   title: "CHORA",
   description: "A NEW SPACE",
